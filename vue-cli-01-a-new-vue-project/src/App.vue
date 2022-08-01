abd46e6536614b7afe1bbfc87d0795a80a9ed0e2
MagicSoft <magicsoft.uz@gmail.com>
2022-08-01 18:56:35 +0500

    Fix error with gitignore

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>My friends</h2>
+    <ul>
+      <li>Salom</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Umid",
+          email: "rustamov@gmail.com",
+          phone: "99983332",
+        },
+        {
+          id: "julia",
+          name: "Julia",
+          email: "julia@gmail.com",
+          phone: "343234234",
+        },
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
