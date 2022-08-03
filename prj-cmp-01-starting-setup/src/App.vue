b9aa0ebf49c3c8ffdb6939f98c3cf5862fb67358
MagicSoft <magicsoft.uz@gmail.com>
2022-08-03 18:58:00 +0500

    Finished LearningResource.vue component

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="resource in resourceData"
+      :key="resource.id"
+      :title="resource.title"
+      :description="resource.description"
+      :link="resource.link"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResourceVue from './components/learning-resource/LearningResource.vue';
+export default {
+  components: {
+    'learning-resource': LearningResourceVue,
+  },
+  data() {
+    return {
+      resourceData: [
+        {
+          id: 'resource-vue',
+          title: 'Resource Vue',
+          description: 'You can learn anything about Vue',
+          link: 'https://vue.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'How to keyword on google',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ prj-cmp-01-starting-setup/src/components/learning-resource/LearningResource.vue
@@ -0,0 +1,23 @@
+<template>
+  <li>
+    <header>
+      <h3>{{ title }}</h3>
+      <button>Delete</button>
+    </header>
+    <main>
+      <p>{{ description }}</p>
+      <a :href="link">Go to</a>
+    </main>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ['title', 'description', 'link'],
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
